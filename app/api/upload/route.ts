e1d50ba081b7d23f9789f3cbed8f8bc836645933
Ilya <piglyuk85@mail.ru>
2026-01-20 13:58:31 +0300

    10

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { writeFile } from 'fs/promises'
-import { open, close } from 'fs/promises'
+import { writeFile, open } from 'fs/promises'
 import path from 'path'
 import { existsSync, mkdirSync } from 'fs'
 
